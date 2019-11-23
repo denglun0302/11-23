0070544a073a2b7b76ff8d1f31441dd9977b985c
denglun0302 <2393751439@qq.com>
2019-11-23 22:14:34 +0800

    提交代码

---
+++ Day0717/src/Day0717.java
@@ -0,0 +1,45 @@
+import java.lang.String;
+public class Day0717 {
+	public static void main (String[]  args){
+	}
+	
+}
+//		int source=0;
+//		int dest=2;
+//		move(2,source,dest);
+//	}
+//}
+		
+//	for(int i=1;i<=9;i++)
+//		{
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.printf("%d*%d=%d  ",i,j,i*j);
+//			}
+//			System.out.println();
+//		}
+//	}
+////	
+//	
+//	
+//   public static void move (int n,int source,int dest){
+//	   int other=3-source-dest;;
+//	   if(n==1)
+//	   {
+//		   System.out.printf("%d->%d%n",source,dest);
+//		   return;
+//	   }
+//	  
+//	   {
+//		   move(n-1,source,other);
+//		   System.out.printf("%d->%d%n",source,dest);
+//		   
+//		   move(n-1,other,dest);
+//
+//	   }
+//	   
+//	   
+//	   
+//   }
+//
+//}
